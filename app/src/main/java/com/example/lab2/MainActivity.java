4aa860071648a10c6aa37886534e968be0c59794
Manya Waldia <waldia@wisc.edu>
2020-02-08 17:45:02 -0600

    Second App BABY

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void clickFunction(View view) {
+        Log.i("Info", "Button Pressed");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
+    }
+
+
 }
